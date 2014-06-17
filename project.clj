7567b2740b5004efcbd881764ea7c0fef1746731
smc <smc@ableton.com>
2014-06-17 18:13:46 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject gol "0.1.0-SNAPSHOT"
+  :description "granular synthesizer"
+  :main gol.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.9.1"]])
---
+++ src/gol/core.clj
@@ -0,0 +1,5 @@
+(ns gol.core
+  (:use [overtone.live]))
+
+(defn run-app []
+  (println "running..."))
